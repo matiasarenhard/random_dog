98e3a5ae2f78c00aa756ece12ca7f57234e5241d
matias <matias_arenhard@hotmail.com>
2025-09-18 22:55:49 -0300

    Rubocop appointments

--- app/controllers/random_dog_controller.rb
+++ app/controllers/random_dog_controller.rb
@@ -1,6 +1,6 @@
 class RandomDogController < ApplicationController
   def index
-    response = Faraday.get('https://dog.ceo/api/breeds/image/random')
-    @dog_url = response.success? ? JSON.parse(response.body)['message'] : nil
+    response = Faraday.get("https://dog.ceo/api/breeds/image/random")
+    @dog_url = response.success? ? JSON.parse(response.body)["message"] : nil
   end
 end
